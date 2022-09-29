e7ee973f9fc20e5fc2189a3fb1e3d042a74920c8
Vcna-0 <36771179+Vcna-0@users.noreply.github.com>
2022-09-29 17:25:19 +0200

    Séparation du code en fonction

--- front/js/index.js
+++ front/js/index.js
@@ -1,29 +1,38 @@
-const addProduct = document.getElementById("items");
+const addProduct = document.querySelector("#items");
 
 // Requête API
 fetch("http://localhost:3000/api/products")
+  .then((response) => response.json())
+  .then((value) => {
+    displayProducts(value);
+  })
+  .catch(console.error);
 
-.then((response) => response.json())
-.then((value) => {
-    for (const object of value) {
+
+// Affiche tous les produits sur la page d'accueil 
+
+function displayProducts (value){
+  for (const object of value) {
       const a = document.createElement("a");
       a.href = "./product.html?id=" + object._id;
       addProduct.appendChild(a);
+
       const article = document.createElement("article");
       a.appendChild(article);
+
       const img = document.createElement("img");
       img.src = object.imageUrl;
       img.alt = object.altTxt;
       article.appendChild(img);
+
       const name = document.createElement("h3");
       name.className = "productName";
       name.textContent = object.name;
       article.appendChild(name);
+
       const p = document.createElement("p");
       p.className = "productDescription";
       p.textContent = object.description;
       article.appendChild(p);
     }
-})
-.catch(console.error);
-
+}
