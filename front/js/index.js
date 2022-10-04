3bbd0052f1477c4bc71e7a1f67699058439ad344
Vcna-0 <36771179+Vcna-0@users.noreply.github.com>
2022-10-04 20:14:05 +0200

    Refactorisation du code

--- front/js/index.js
+++ front/js/index.js
@@ -1,38 +1,23 @@
-const addProduct = document.querySelector("#items");
+const sectionItems = document.querySelector('#items');
 
 // Requête API
-fetch("http://localhost:3000/api/products")
+fetch('http://localhost:3000/api/products')
   .then((response) => response.json())
   .then((value) => {
     displayProducts(value);
   })
   .catch(console.error);
 
-
-// Affiche tous les produits sur la page d'accueil 
-
-function displayProducts (value){
+function displayProducts(value) {
   for (const object of value) {
-      const a = document.createElement("a");
-      a.href = "./product.html?id=" + object._id;
-      addProduct.appendChild(a);
-
-      const article = document.createElement("article");
-      a.appendChild(article);
-
-      const img = document.createElement("img");
-      img.src = object.imageUrl;
-      img.alt = object.altTxt;
-      article.appendChild(img);
-
-      const name = document.createElement("h3");
-      name.className = "productName";
-      name.textContent = object.name;
-      article.appendChild(name);
-
-      const p = document.createElement("p");
-      p.className = "productDescription";
-      p.textContent = object.description;
-      article.appendChild(p);
-    }
+    const a = document.createElement('a');
+    a.href = './product.html?id=' + object._id;
+    let htmlelement = ` <article>
+                          <img src="${object.imageUrl}" alt="${object.altTxt}">
+                          <h3 class="productName">${object.name}</h3>
+                          <p class="productDescription">${object.description}</p>
+                        </article>`;
+    a.innerHTML = htmlelement;
+    sectionItems.appendChild(a);
+  }
 }
